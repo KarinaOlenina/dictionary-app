abfc42ebe966289914e0365e556f3a6a19e3147c
KarinaOlenina <ua.olenina@gmail.com>
2022-09-26 16:53:54 +0300

    install create react-app, create file structure, and add node-sass

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <div className="wrap">
+
+
+            </div>
+        </div>
+    );
+}
+
+export default App;

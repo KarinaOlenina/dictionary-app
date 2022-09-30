7520cb7039883e2c6a3036dd47b135f4bfecd2df
KarinaOlenina <ua.olenina@gmail.com>
2022-09-30 11:47:49 +0300

    fixed react-router (Link)

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import logo from '../../assets/logo.png'
 import './Navbar.scss';
-import Link from "./Link/Link";
+import Links from "./Link/Link";
 
 const Navbar = () => {
 
@@ -17,10 +17,10 @@ const Navbar = () => {
                     </a>
                 </div>
                 <ul className={'nav_list'}>
-                    <Link path={'/'} name={'Головна'}/>
-                    <Link path={'/add_words'} name={'Додати слово'}/>
-                    <Link path={'/test'} name={'Перевір себе'}/>
-                    <Link path={'/stats'} name={'Статистика'}/>
+                    <Links path={'/'} name={'Головна'}/>
+                    <Links path={'/add_words'} name={'Додати слово'}/>
+                    <Links path={'/test'} name={'Перевір себе'}/>
+                    <Links path={'/stats'} name={'Статистика'}/>
                 </ul>
             </div>
         </nav>
